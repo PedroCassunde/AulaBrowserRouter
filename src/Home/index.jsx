22919a0e6fd79981db7c158c1c9ca08a4e7139e8
PedroCassunde <pedro.lima4@estudante.ifms.edu.br>
2024-08-14 16:06:30 -0400

    nightcrawling

---
+++ src/Home/index.jsx
@@ -0,0 +1,10 @@
+import Header fom "../components/Header";
+import { Ca}
+export default function Home(){
+    return(
+        <>
+            <Header/>
+            <p>Zombie</p>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <BrowseRoute>
+    <Route>
+      <Route path="" element ={<Home/>}/>
+      <Route path="" element ={<Oferta/>}/>
+    </Route>
+    </BrowseRoute>    
   </React.StrictMode>,
 )
-
-
-
